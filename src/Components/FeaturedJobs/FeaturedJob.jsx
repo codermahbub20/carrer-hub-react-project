4ff0660afb3c35b294610cb9aadb84c1e249e92c
codermahbub20 <mkrefat5@gmail.com>
2023-09-22 00:50:18 +0600

    Sucessfully data load in jobs.json file

--- src/Components/FeaturedJobs/FeaturedJob.jsx
+++ src/Components/FeaturedJobs/FeaturedJob.jsx
@@ -1,11 +1,25 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
 
 
 const FeaturedJob = () => {
+
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('Jobs.json')
+        .then(res => res.json())
+        .then(data => setJobs(data))
+    },[])
+
     return (
         <div>
             <div>
-                <h1 className='text-5xl text-center'>Featured Jobs</h1>
+                <h1 className='text-5xl text-center'>Featured Jobs : {jobs.length}</h1>
                 <p className="text-center">Explore thousands of job opportunities with all the information you need. Its your future</p>
+                {
+                    jobs.map(job => <Job key={job.id} job={job}></Job>)
+                }
             </div>
         </div>
     );
